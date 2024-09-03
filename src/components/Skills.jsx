55abad3e5b0d1a755f1356984da24efe88bc16c4
Gene Howell <gene.howell9@gmail.com>
2024-09-02 23:50:22 -0500

    adjusted top margin on the skills section for the about page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const Skills = () => {
   return (
     <>
-      <div className="my-64 w-full xs:!mt-12">
+      <div className="my-64 w-full xs:!mt-14">
         <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
           Skills
         </h2>
