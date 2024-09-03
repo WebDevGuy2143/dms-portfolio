1807b327a6b7477aff52c8519cfd9c9ba91f73b4
Gene Howell <gene.howell9@gmail.com>
2024-09-02 23:57:08 -0500

    adjusted the margin on the experience section for the about page

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -28,7 +28,7 @@ const Details = ({ position, company, companyLink, time, address, work }) => {
 const Experience = () => {
   return (
     <div className="my-64">
-      <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
+      <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl xs:!mt-14 xs:!mb-14 md:mb-16">
         Experience
       </h2>
       <div className="w-[75%] mx-auto relative lg:[90%] md:w-full">
