9408db7799bcaa7108c881674561f0d43df27733
Gene Howell <gene.howell9@gmail.com>
2024-09-12 14:02:41 -0500

    updated sizing on quote for mobile on main articles page

--- src/pages/Blog/articles.jsx
+++ src/pages/Blog/articles.jsx
@@ -68,7 +68,7 @@ const articles = () => {
         <Layout className="pt-16">
           <AnimatedText
             text="“Great things are done by a series of small things brought together.” – Vincent Van Gogh"
-            className="mb-16 !text-6xl !text-center xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl sm:mb-16"
+            className="mb-16 !text-6xl !text-center xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl sm:mb-16 xs:!text-xl"
           />
           <ul className="grid grid-cols-2 gap-16 lg:gap-8 md:grid-cols-1 md:gap-y-16">
             <FeaturedArticle
