6d542b0ea9267c9b9c4de4dc89ceaabe6bdc238d
Gene Howell <gene.howell9@gmail.com>
2024-12-07 12:43:45 -0600

    Update Experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -98,10 +98,10 @@ const Experience = () => {
             position={"Student Assistant"}
             company={"Gulf Coast State College"}
             companyLink={"https://centerforstudentmedia.com/"}
-            time={"2023-Present"}
+            time={"2023-2024"}
             address={"Panama City, Fl"}
             work={
-              "I am currently working as a student assistant for the Digital Media and Design program. During my timeframe, I am participating in helping the program with areas such as graphic design, web development, radio broadcasting, video production (editing and camera work)."
+              "I worked as a student assistant for the Digital Media and Design program. During my timeframe, I am participating in helping the program with areas such as graphic design, web development, radio broadcasting, video production (editing and camera work)."
             }
           />
         </ul>
