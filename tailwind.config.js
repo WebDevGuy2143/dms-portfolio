e98442bf78b8f1bef1839eaebb772f0176c7c52f
Gene Howell <gene.howell9@gmail.com>
2024-12-08 10:42:24 -0600

    change background for lightMode

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
       },
       colors: {
         dark: "#141925",
-        light: "rgb(255 251 235)",
+        light: "bg-purple-100",
         moreLight: "slate-gray-500",
         primary: "#B63E96", // 240,86,199
         primaryDark: "#58E6D9", // 80,230,217},
