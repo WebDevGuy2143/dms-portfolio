35e9c777727875aae7dd47fe030b4039a75a0f28
Gene Howell <gene.howell9@gmail.com>
2024-12-08 11:39:57 -0600

    change bg for lightmode in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
       },
       colors: {
         dark: "#303030",
-        light: "bg-purple-200",
+        light: "bg-purple-300",
         moreLight: "slate-gray-500",
         primary: "#B63E96", // 240,86,199
         primaryDark: "#58E6D9", // 80,230,217},
