2cb1fa1a3bf003b19b1c3abf7f412910c74a9b7e
Gene Howell <gene.howell9@gmail.com>
2024-12-08 18:59:55 -0600

    Adjusted font size for paragraph tags on about page

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -31,19 +31,19 @@ const about = () => {
               <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light">
                 My Bio
               </h2>
-              <p className="font-medium pb-3">
+              <p className="text-xl font-medium pb-3">
               I have been passionate about technology since 2009, specializing in web development. My journey began at Chattahoochee Technical College in Marietta, Georgia, where I started as a freelance web developer. I later transferred to Gulf Coast State College in Panama City, Florida, expanding my expertise in website creation, and modern development methodologies.
               </p>
-              <p className="font-medium pb-3">
+              <p className="text-xl font-medium pb-3">
               Upon completion of my Digital Media Associate&apos;s and Bachelor&apos;s Degree, I acquired versatile skills encompassing Graphic Design, Programming, Videography, and Audio Engineering, enhancing my well-rounded industry profile.
               </p>
-              <p className="font-medium pb-3">
+              <p className="text-xl font-medium pb-3">
               My core coding competencies include HTML, CSS, JavaScript, PHP (with WordPress), Python, and C++. I am also proficient in popular frameworks such as React, Laravel, Bootstrapm, and TailwindCSS, as well as environments like NodeJS, Composer, Laravel, and Yarn.
               </p>
-              <p className="font-medium pb-3">
+              <p className="text-xl font-medium pb-3">
               Embracing the dynamic nature of the industry, I continuously update my skills to create cutting-edge, standards-compliant websites.
               </p>
-              <p className="font-medium">
+              <p className=" text-xlfont-medium">
               To connect, please reach me at genehowellwebguy@gmail.com or 404-310-4661.
               </p>
 
