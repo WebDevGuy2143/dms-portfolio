9b7f8a6fca8f5ce07775c811b89a0be3f658d19d
Gene Howell <gene.howell9@gmail.com>
2024-12-08 18:10:47 -0600

    re-adjusted content width of about page for top section

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -59,30 +59,6 @@ const about = () => {
               />
             </div>
             <div className="col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:!order-3">
-              <div className="flex flex-col items-end justify-center xl:items-center">
-                <span className="inline-block text-7xl font-bold md:text-4xl sm:text-3xl xs:text-2xl">
-                  50+
-                </span>
-                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">
-                  Satisfied Clients
-                </h2>
-              </div>
-              <div className="flex flex-col items-end justify-center xl:items-center">
-                <span className="inline-block text-7xl font-bold md:text-4xl sm:text-3xl xs:text-2xl">
-                  40+
-                </span>
-                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">
-                  Projects Completed
-                </h2>
-              </div>
-              <div className="flex flex-col items-end justify-center xl:items-center">
-                <span className="inline-block text-7xl font-bold md:text-4xl sm:text-3xl xs:text-2xl">
-                  4+
-                </span>
-                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">
-                  Years of Experience
-                </h2>
-              </div>
             </div>
           </div>
           <Skills />
