8635e767d7a6684e11b29380640fdbc7f7da9791
Gene Howell <gene.howell9@gmail.com>
2024-12-08 18:04:01 -0600

    re-worded about me copy on about page

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -32,19 +32,19 @@ const about = () => {
                 My Bio
               </h2>
               <p className="font-medium pb-3">
-                I knew I always had a passion for technology. Little did I know it would be in the web development field. Ever since 2009, I had started my journey with college at Chattahoochee Technical collage in Marietta, Ga as a freelance web developer. Ever since then I&apos;ve transferred to Gulf Coast State College in Panama City, Florida where I&apos;ve gained a lot more experience in how websites are made, the theory behind how browsers retrieve all the ins-and-outs of a website, as well as the fast paced advancements of how websites are created today in the development phase.
+              I have been passionate about technology since 2009, specializing in web development. My journey began at Chattahoochee Technical College in Marietta, Georgia, where I started as a freelance web developer. I later transferred to Gulf Coast State College in Panama City, Florida, expanding my expertise in website creation, and modern development methodologies.
               </p>
               <p className="font-medium pb-3">
-                Along with my Digital Media associates degree that I will obtain at the end of this year, I&apos;ve picked up some other useful skills that have made me a well rounded person in the IT industry. Those skills consist of Graphic Design, Programming, Videography, Audio engineering, and much more.
+              Upon completion of my Digital Media Associate&apos;s and Bachelor&apos;s Degree, I acquired versatile skills encompassing Graphic Design, Programming, Videography, and Audio Engineering, enhancing my well-rounded industry profile.
               </p>
               <p className="font-medium pb-3">
-                With that all being said, my coding specialties involve, HTML, CSS, JavaScript, PHP (w/WordPress), Python and C++. With those coding specialties, I also have knowledge in frameworks, such as React, Laravel, and Bootstrap. I also have knowledge with environments such as NodeJS, Composer, and Yarn.
+              My core coding competencies include HTML, CSS, JavaScript, PHP (with WordPress), Python, and C++. I am also proficient in popular frameworks such as React, Laravel, Bootstrapm, and TailwindCSS, as well as environments like NodeJS, Composer, Laravel, and Yarn.
               </p>
               <p className="font-medium pb-3">
-                As with many in this industry, it is never stagnant. So I am forever learning and implementing the new skillsets that I learn along the way with creating &quot;up to code&quot;, websites that meet todays standards.
+              Embracing the dynamic nature of the industry, I continuously update my skills to create cutting-edge, standards-compliant websites.
               </p>
               <p className="font-medium">
-                Get in touch via email at: genehowellwebguy@gmail.com or phone: 404-310-4661
+              To connect, please reach me at genehowellwebguy@gmail.com or 404-310-4661.
               </p>
 
             </div>
