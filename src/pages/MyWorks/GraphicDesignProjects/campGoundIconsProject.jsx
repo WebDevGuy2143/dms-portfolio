8de973e58bd94bcab39780095ca9405bb0d4f138
Gene Howell <gene.howell9@gmail.com>
2025-01-31 16:55:33 -0600

    HTML entities fix apostrophe

--- src/pages/MyWorks/GraphicDesignProjects/campGoundIconsProject.jsx
+++ src/pages/MyWorks/GraphicDesignProjects/campGoundIconsProject.jsx
@@ -20,14 +20,14 @@ const campgroundIconsProject = () => {
       <main className="flex w-full flex-col items-center justify-center dark:text-light">
         <Layout className="pt-16">
           <AnimatedText
-            text='&ldquo;Don&rsquo;t wait for inspiration. It comes while working." &mdash; Henri Matisse&rdquo;'
+            text="&ldquo;Don't wait for inspiration. It comes while working.&quot; &mdash; Henri Matisse&rdquo;"
             className="mb-16 !text-6xl !text-center xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
           />
           <div className="grid w-full grid-cols-8 gap-16 sm:gap-8">
             <div className="col-span-12 flex flex-col items-start justify-start xl:col-span-4 md:!order-2 md:!col-span-8">
               <div className="container py-8">
                 <h1 className="font-bold text-5xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-164">
-                  Cooper&lsquo;s Campground Icons Project Timeline
+                  Cooper&apos;s Campground Icons Project Timeline
                 </h1>
                 <p className="sm:text-sm text-xl font-medium pb-3">
                   Client Brief copy goes here...
